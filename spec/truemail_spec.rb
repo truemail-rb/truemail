195aca8e4d856753a4ca6a61ac2efc77e126ce54
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-03-04 18:38:39 +0200

    Initialize rspec

---
+++ spec/truemail_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Truemail do
+  it 'has a version number' do
+    expect(Truemail::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
