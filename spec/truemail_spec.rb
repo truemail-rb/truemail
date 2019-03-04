f64d4c7a2b8b93183b7ec41559c50bb5224e3b25
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-03-04 19:22:27 +0200

    Fix default tests

--- spec/truemail_spec.rb
+++ spec/truemail_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe Truemail do
   it 'has a version number' do
-    expect(Truemail::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(Truemail::VERSION).not_to be(nil)
   end
 end
