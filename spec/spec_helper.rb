dae633c6acb1e30e261917f996da5c37426c6313
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-03-09 22:16:53 +0200

    Fix rspec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,9 @@ rspec_custom = File.join(File.dirname(__FILE__), 'support/**/*.rb')
 Dir[File.expand_path(rspec_custom)].each { |file| require file }
 
 RSpec.configure do |config|
-  config.include ConfigurationHelper
   config.example_status_persistence_file_path = '.rspec_status'
   config.disable_monkey_patching!
+  config.include Truemail::ConfigurationHelper
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
--- spec/support/helpers/configuration_helper.rb
+++ spec/support/helpers/configuration_helper.rb
@@ -1,8 +1,10 @@
-module ConfigurationHelper
-  def configuration_block(**configuration_settings)
-    lambda do |config|
-      configuration_settings.each do |attribute, value|
-        config.public_send(:"#{attribute}=", value)
+module Truemail
+  module ConfigurationHelper
+    def configuration_block(**configuration_settings)
+      lambda do |config|
+        configuration_settings.each do |attribute, value|
+          config.public_send(:"#{attribute}=", value)
+        end
       end
     end
   end
