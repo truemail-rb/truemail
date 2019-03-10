0e18a8d59950a153119f024dedb509f94933dd47
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-03-10 16:59:20 +0200

    Fix rspec helpers

--- spec/support/shared_examples/has_attr_accessor.rb
+++ spec/support/shared_examples/has_attr_accessor.rb
@@ -1,8 +1,10 @@
-RSpec.shared_examples 'has attr_accessor' do
-  %i[email_pattern verifier_email verifier_domain].each do |attribute|
-    it "has attr_accessor :#{attribute}" do
-      expect(configuration_instance.respond_to?(attribute)).to be(true)
-      expect(configuration_instance.respond_to?(:"#{attribute}=")).to be(true)
+module Truemail
+  RSpec.shared_examples 'has attr_accessor' do
+    %i[email_pattern verifier_email verifier_domain].each do |attribute|
+      it "has attr_accessor :#{attribute}" do
+        expect(configuration_instance.respond_to?(attribute)).to be(true)
+        expect(configuration_instance.respond_to?(:"#{attribute}=")).to be(true)
+      end
     end
   end
 end
