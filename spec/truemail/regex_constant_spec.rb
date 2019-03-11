b91faf109fe47e317f5da59ef8251dde0f850396
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-03-11 16:43:51 +0200

    Complete RegexConstant tests

--- spec/truemail/regex_constant_spec.rb
+++ spec/truemail/regex_constant_spec.rb
@@ -33,5 +33,41 @@ module Truemail
         ).to be(false)
       end
     end
+
+    describe 'REGEX_DOMAIN_PATTERN' do
+      subject(:regex_pattern) { described_class::REGEX_DOMAIN_PATTERN }
+
+      it 'allows from 4 to 255 chars' do
+        expect(regex_pattern.match?('i.us')).to be(true)
+        expect(regex_pattern.match?("#{'i' * 252}.us")).to be(true)
+      end
+
+      it 'allows numbers, letters, dashs' do
+        expect(regex_pattern.match?('1.us')).to be(true)
+        expect(regex_pattern.match?('l.us')).to be(true)
+        expect(regex_pattern.match?('1domain.us')).to be(true)
+        expect(regex_pattern.match?('1-domain.us')).to be(true)
+      end
+
+      it 'allows nested subdomains' do
+        expect(regex_pattern.match?('42.com')).to be(true)
+        expect(regex_pattern.match?('42.subdomain.domain')).to be(true)
+        expect(regex_pattern.match?('service.subdomain.company.domain')).to be(true)
+      end
+
+      it 'allows tld size between 2 and 7 chars' do
+        expect(regex_pattern.match?('domain.io')).to be(true)
+        expect(regex_pattern.match?('domain.rentals')).to be(true)
+      end
+
+      it 'not allows dash as last char' do
+        expect(regex_pattern.match?('1_.us')).to be(false)
+        expect(regex_pattern.match?('1_.com_')).to be(false)
+      end
+
+      it 'not allows number in tld' do
+        expect(regex_pattern.match?('domain.42')).to be(false)
+      end
+    end
   end
 end
