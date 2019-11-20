5348f97e670d490fedf83bb2e653bf5dfe99bd07
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-11-20 21:26:40 +0200

    Technical/Switch to truemail-rspec (#59)

--- spec/support/helpers/generate_email_helper_spec.rb
+++ spec/support/helpers/generate_email_helper_spec.rb
@@ -1,48 +1,46 @@
 # frozen_string_literal: true
 
-module Truemail
-  RSpec.describe GenerateEmailHelper, type: :helper do
-    describe '.call' do
-      context 'without params' do
-        subject(:generate_email) { described_class.call }
+RSpec.describe Truemail::GenerateEmailHelper, type: :helper do # rubocop:disable RSpec/FilePath
+  describe '.call' do
+    context 'without params' do
+      subject(:generate_email) { described_class.call }
 
-        let(:allowed_symbols) { %w[@ - _ . +] }
+      let(:allowed_symbols) { %w[@ - _ . +] }
 
-        specify { expect(generate_email).to be_an_instance_of(String) }
-        specify { 100.times { expect(generate_email.size).to be_between(15, 255) } }
-        specify { 100.times { expect(generate_email[0]).not_to include(*allowed_symbols) } }
-        specify { 100.times { expect(generate_email).to include(*allowed_symbols) } }
-      end
+      specify { expect(generate_email).to be_an_instance_of(String) }
+      specify { 100.times { expect(generate_email.size).to be_between(15, 255) } }
+      specify { 100.times { expect(generate_email[0]).not_to include(*allowed_symbols) } }
+      specify { 100.times { expect(generate_email).to include(*allowed_symbols) } }
+    end
 
-      context 'with size: :min' do
-        specify { expect(described_class.call(size: :min).size).to eq(6) }
-        specify { expect(described_class.call(size: :min)[0]).to match(/[a-z]/) }
-      end
+    context 'with size: :min' do
+      specify { expect(described_class.call(size: :min).size).to eq(6) }
+      specify { expect(described_class.call(size: :min)[0]).to match(/[a-z]/) }
+    end
 
-      context 'with size: :max' do
-        specify { expect(described_class.call(size: :max).size).to eq(255) }
-      end
+    context 'with size: :max' do
+      specify { expect(described_class.call(size: :max).size).to eq(255) }
+    end
 
-      context 'with size: :out_of_range' do
-        specify { expect(described_class.call(size: :out_of_range).size).to be > 255 }
-      end
+    context 'with size: :out_of_range' do
+      specify { expect(described_class.call(size: :out_of_range).size).to be > 255 }
+    end
 
-      context 'when invalid_email_with exists' do
-        let(:not_allowed_symbols) { %w[! ~ , ' & %] }
+    context 'when invalid_email_with exists' do
+      let(:not_allowed_symbols) { %w[! ~ , ' & %] }
 
-        specify do
-          100.times do
-            expect(
-              described_class.call(invalid_email_with: not_allowed_symbols)
-            ).to include(*not_allowed_symbols)
-          end
-        end
-
-        specify do
+      specify do
+        100.times do
           expect(
-            described_class.call(size: :min, invalid_email_with: not_allowed_symbols)
-          ).to match(/\W/)
+            described_class.call(invalid_email_with: not_allowed_symbols)
+          ).to include(*not_allowed_symbols)
         end
+      end
+
+      specify do
+        expect(
+          described_class.call(size: :min, invalid_email_with: not_allowed_symbols)
+        ).to match(/\W/)
       end
     end
   end
--- spec/truemail/auditor_spec.rb
+++ spec/truemail/auditor_spec.rb
@@ -1,42 +1,40 @@
 # frozen_string_literal: true
 
-module Truemail
-  RSpec.describe Truemail::Auditor do
-    subject(:auditor_instance) { described_class.new(configuration: configuration_instance) }
+RSpec.describe Truemail::Auditor do
+  subject(:auditor_instance) { described_class.new(configuration: configuration_instance) }
 
-    let(:configuration_instance) { create_configuration }
+  let(:configuration_instance) { create_configuration }
 
-    describe 'defined constants' do
-      specify { expect(described_class).to be_const_defined(:Result) }
-    end
+  describe 'defined constants' do
+    specify { expect(described_class).to be_const_defined(:Result) }
+  end
 
-    describe '.new' do
-      it 'creates auditor with result getter' do
-        expect(auditor_instance.result).to be_an_instance_of(Truemail::Auditor::Result)
-      end
-    end
-
-    describe '#run' do
-      it 'runs audition methods' do
-        expect(Truemail::Audit::Ptr).to receive(:check)
-        expect(auditor_instance.run).to be_an_instance_of(described_class)
-      end
+  describe '.new' do
+    it 'creates auditor with result getter' do
+      expect(auditor_instance.result).to be_an_instance_of(Truemail::Auditor::Result)
     end
   end
 
-  RSpec.describe Truemail::Auditor::Result do
-    subject(:result_instance) { described_class.new }
-
-    it 'has attribute accessors warnings, configuration' do
-      expect(result_instance.members).to include(:warnings, :configuration)
+  describe '#run' do
+    it 'runs audition methods' do
+      expect(Truemail::Audit::Ptr).to receive(:check)
+      expect(auditor_instance.run).to be_an_instance_of(described_class)
     end
+  end
+end
 
-    it 'has default values for attributes' do
-      expect(result_instance.warnings).to eq({})
-    end
+RSpec.describe Truemail::Auditor::Result do
+  subject(:result_instance) { described_class.new }
 
-    it 'accepts parametrized arguments' do
-      expect(described_class.new(warnings: :data).warnings).to eq(:data)
-    end
+  it 'has attribute accessors warnings, configuration' do
+    expect(result_instance.members).to include(:warnings, :configuration)
+  end
+
+  it 'has default values for attributes' do
+    expect(result_instance.warnings).to eq({})
+  end
+
+  it 'accepts parametrized arguments' do
+    expect(described_class.new(warnings: :data).warnings).to eq(:data)
   end
 end
