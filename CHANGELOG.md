db33de154597292424cedd1d65ec688542aa73e3
Vladislav Trotsenko <admin@bestweb.com.ua>
2022-11-01 14:53:52 +0100

    Bugfix/Wrong semantic version comparison in gemspec runtime dependency definition (#233)
    * Updated development dependencies
    * Updated gemspecs
    * Updated RSpec helper
    * Updated CircleCI config
    * Updated gem version, docs, changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,18 @@
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [3.0.1] - 2022.11.01
+
+### Fixed
+
+- Fixed wrong semantic version comparison in gemspec runtime dependency definition
+
+### Updated
+
+- Updated RSpec helpers
+- Updated gemspecs, circleci config
+- Updated gem version
+
 ## [3.0.0] - 2022.10.24
 
 ### Added
--- spec/support/config/truemail.rb
+++ spec/support/config/truemail.rb
@@ -2,33 +2,6 @@
 
 require 'truemail/rspec'
 
-# TODO: remove this part in next release
-module Truemail
-  module RSpec
-    module ValidatorHelper
-      VALIDATION_LIST_TYPE_REGEX_PATTERN = /(emails|domains)(_list)/.freeze
-
-      class ValidatorFactory
-        private
-
-        def process_validator_params # rubocop:disable Metrics/AbcSize
-          case validation_type
-          when Truemail::RSpec::ValidatorHelper::VALIDATION_LIST_TYPE_REGEX_PATTERN
-            list_type = validation_type[Truemail::RSpec::ValidatorHelper::VALIDATION_LIST_TYPE_REGEX_PATTERN, 1]
-            self.validation_type = nil
-            method = success ? :"whitelisted_#{list_type}" : :"blacklisted_#{list_type}"
-            configuration.tap do |config|
-              config.public_send(method) << (list_type.eql?('emails') ? email : email[Truemail::RegexConstant::REGEX_EMAIL_PATTERN, 3])
-            end
-          when :mx_blacklist
-            configuration.blacklisted_mx_ip_addresses.push(*mail_servers) unless success
-          end
-        end
-      end
-    end
-  end
-end
-
 RSpec.configure do |config|
   config.include Truemail::RSpec
 end
